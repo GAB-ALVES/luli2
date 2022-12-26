6a123908a0cd7513eed6f1fe41290e7b89c269fb
Ga-Alves <gabrielalvesreis2gmail.com>
2022-12-25 22:38:13 -0300

    feat: banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
-import NavBar from "../../components/NavBar/NavBar"
+import './Home.scss'
 
+import NavBar from "../../components/NavBar/NavBar"
+import Banner from '../../utils/media/imgs/Banner.svg'
 function Home() {
     return (
         <div className='Home'>
             <NavBar/>
+            <img className="Banner" src={Banner} alt="" />
             hello word!!!!
         </div>
     )
